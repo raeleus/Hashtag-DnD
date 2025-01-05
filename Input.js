0102b9ea56ef1521bb46fa35bb8f3b6fc64ba445
raeleus <raymond.ray3k@gmail.com>
2025-01-04 17:47:30 -0800

    Update version to 0.5.0

--- Input.js
+++ Input.js
@@ -1,4 +1,4 @@
-const version = "Hashtag DnD v0.4.0"
+const version = "Hashtag DnD v0.5.0"
 const rollSynonyms = ["roll"]
 const createSynonyms = ["create", "generate", "start", "begin", "setup", "party", "member", "new"]
 const renameCharacterSynonyms = ["renamecharacter", "renameperson"]
